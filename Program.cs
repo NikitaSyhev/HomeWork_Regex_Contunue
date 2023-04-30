408685cc75c7b3a767f4ccc7b372501a5707295f
Nikita Sychev <nikitasychev@inbox.ru>
2023-04-30 13:32:15 +0500

    task 1 done

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace HomeWork_Regex_Contunue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Task1");
+            Console.WriteLine("Task 1.1");
             {
                 //Измените код таким образом, чтобы при вводе «Program.exe 123s dgfs  123 12» он нашёл 3 числа - 123 123 и 12.
 
@@ -31,8 +31,22 @@ namespace HomeWork_Regex_Contunue
 
 
                 // Измените код таким образом, чтобы он корректно принимал отрицательные числа типа int.
+                Console.WriteLine("Task 1.2");
+                string input1 = "123s dgfs  123 12 ";
+                for (int i = 0; i <= input1.Length; i++)
+                {
+                    input1 = args[i];
+                    Regex regex = new Regex(@"((-)+\d+)");
+                    MatchCollection matchFind = regex.Matches(input1);
 
+                    foreach (var item in matchFind)
+                    {
+                        Console.WriteLine($"Совпадение  {item}");
+                    }
 
+                }
+
+                Console.WriteLine("Task 2.0");
 
 
 
